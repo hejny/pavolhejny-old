1f806eda77f40d5771ca0f0e5e1ba5523d4ec42b
hejny <me@pavolhejny.com>
2017-05-06 02:23:47 +0200

    bugfixes

--- src/script/server.jsx
+++ src/script/server.jsx
@@ -164,12 +164,17 @@ app.get('/*', function (req, res) {
 
         if(HOSTNAME) {
             for (let alias in HOSTNAME_ALIASES) {
+                console.log(req.hostname,alias);
                 if (req.hostname === alias) {
-                    res.redirect(301, `https://${HOSTNAME}${HOSTNAME_ALIASES[alias]}`);
+                    res.redirect(301, `//${HOSTNAME}${HOSTNAME_ALIASES[alias]}`);
                     return;
                 }
             }
+
+            res.redirect(301, `//${HOSTNAME}`);
+            return;
         }
+
 
 
         const normalizedUri = createUriFromState(PERSONAL,state);
