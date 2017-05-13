30ab6bd933bf4a3a164b17ef5f9627b4530a219b
hejny <me@pavolhejny.com>
2017-05-13 02:50:05 +0200

    security -  quotes

--- src/script/server.jsx
+++ src/script/server.jsx
@@ -20,7 +20,7 @@ app.use(helmet.contentSecurityPolicy({
     directives: {
         defaultSrc: ["'self'"],
         scriptSrc: ["'self'",'www.google-analytics.com',(req, res)=>res.locals.nonce],
-        styleSrc: ["'self'", 'unsafe-inline', 'maxcdn.bootstrapcdn.com', 'cdnjs.cloudflare.com'],
+        styleSrc: ["'self'", "'unsafe-inline'", 'maxcdn.bootstrapcdn.com', 'cdnjs.cloudflare.com'],
         fontSrc: ["'self'", 'maxcdn.bootstrapcdn.com', 'cdnjs.cloudflare.com'],
         frameSrc: ["'self'", '*'],
         imgSrc: ["'self'",'data:','*'],
