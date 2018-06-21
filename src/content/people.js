3343d9c5dd42342211f82a04d3cc74799760ac69
Pavol Hejny <me@pavolhejny.com>
2018-06-21 16:31:20 +0200

    people

---
+++ src/content/people.js
@@ -0,0 +1,22 @@
+export const PEOPLE = [
+    {
+        name: 'David Žahour',
+        link: 'https://www.facebook.com/isouesense/',
+    },
+    {
+        name: 'Vladimír Smitka',
+        link: 'https://www.linkedin.com/in/vsmitka/',
+    },
+    {
+        name: 'Pavel Koenig',
+        link: 'https://www.linkedin.com/in/pavelkoenig/',
+    },
+    {
+        name: 'Max Kozlov',
+        link: 'https://www.linkedin.com/in/themaxkozlov/',
+    },
+    {
+        name: 'Jan Steinbach',
+        link: 'https://www.linkedin.com/in/honzasteinbach/',
+    },
+];
--- src/script/components/person.js
+++ src/script/components/person.js
@@ -1,32 +1,10 @@
 import { findInLibrary } from '../findInLibrary';
-
-const people = [
-    {
-        name: 'David Žahour',
-        link: 'https://www.facebook.com/isouesense/',
-    },
-    {
-        name: 'Vladimír Smitka',
-        link: 'https://www.linkedin.com/in/vsmitka/',
-    },
-    {
-        name: 'Pavel Koenig',
-        link: 'https://www.linkedin.com/in/pavelkoenig/',
-    },
-    {
-        name: 'Max Kozlov',
-        link: 'https://www.linkedin.com/in/themaxkozlov/',
-    },
-    {
-        name: 'Jan Steinbach',
-        link: 'https://www.linkedin.com/in/honzasteinbach/',
-    },
-];
+import { PEOPLE } from '../../content/people';
 
 export function processPeople() {
     for (const personElement of document.querySelectorAll('person')) {
         const originalID = personElement.innerHTML;
-        const person = findInLibrary(originalID, people);
+        const person = findInLibrary(originalID, PEOPLE);
 
         if (person) {
             const [originalIDName, originalIDRole] = originalID.split('<role>');
